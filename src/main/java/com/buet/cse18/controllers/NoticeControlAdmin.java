2ccac9063ecd69cb8e957a5294a9d6c9ee9c7044
Amimul <amimulahsan7@gmail.com>
2021-03-09 12:49:55 +0600

    Admin, Viewer image controlling added. And notice description also added.

--- src/main/java/com/buet/cse18/controllers/NoticeControlAdmin.java
+++ src/main/java/com/buet/cse18/controllers/NoticeControlAdmin.java
@@ -1,18 +1,29 @@
 package com.buet.cse18.controllers;
 
 import com.buet.cse18.entity.Notice;
+import com.buet.cse18.services.ImageControllingService;
 import com.buet.cse18.services.NoticeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+
 
 @RestController
 @RequestMapping(path = "/admin/noticeBoard/v1")
 public class NoticeControlAdmin {
-
     private final NoticeService noticeService;
+    private final ImageControllingService imageControllingService;
 
-    public NoticeControlAdmin(NoticeService noticeService) {
+    public NoticeControlAdmin(NoticeService noticeService, ImageControllingService imageControllingService) {
         this.noticeService = noticeService;
+        this.imageControllingService = imageControllingService;
     }
 
     @PostMapping(path = "/createNewNotice")
@@ -44,4 +55,24 @@ public class NoticeControlAdmin {
     {
         return noticeService.searchForNotices(searchQuery);
     }
+
+    @PostMapping("/imageController/{directory}")
+    public String saveImageInProject(@PathVariable("directory") String directory,
+                                     @RequestPart(name = "multipartFile") MultipartFile multipartFile)
+    {
+        return imageControllingService.saveImageInFolder(directory, multipartFile);
+    }
+
+    @DeleteMapping("/getImageFromServer")
+    public String deleteImage(@RequestParam(name = "path_of_image") String path_of_image)
+    {
+        return imageControllingService.deleteImage(path_of_image);
+    }
+
+    @GetMapping(value = "/getImageFromServer", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<?> sendImageToClient(@RequestParam(name = "path_of_image") String path_of_image)
+    {
+        return imageControllingService.getImageFromServer(path_of_image);
+    }
+
 }
--- src/main/java/com/buet/cse18/controllers/NoticeControlViewer.java
+++ src/main/java/com/buet/cse18/controllers/NoticeControlViewer.java
@@ -1,7 +1,10 @@
 package com.buet.cse18.controllers;
 
 import com.buet.cse18.entity.Notice;
+import com.buet.cse18.services.ImageControllingService;
 import com.buet.cse18.services.NoticeService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +16,11 @@ import java.util.List;
 public class NoticeControlViewer {
 
     private final NoticeService noticeService;
+    private final ImageControllingService imageControllingService;
 
-    public NoticeControlViewer(NoticeService noticeService) {
+    public NoticeControlViewer(NoticeService noticeService, ImageControllingService imageControllingService) {
         this.noticeService = noticeService;
+        this.imageControllingService = imageControllingService;
     }
 
     @GetMapping(path = "/getCurrentNotices")
@@ -28,5 +33,11 @@ public class NoticeControlViewer {
     public List<Notice> searchForNotices(@RequestParam("searchQuery") String searchQuery)
     {
         return noticeService.searchForNotices(searchQuery);
+    }
+
+    @GetMapping(value = "/getImageFromServer", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<?> sendImageToClient(@RequestParam(name = "path_of_image") String path_of_image)
+    {
+        return imageControllingService.getImageFromServer(path_of_image);
     }
 }
